9e797041cf1e5b69a5fa9f2b857a64ab76dac340
Thomas Boerger <thomas@webhippie.de>
2025-02-04 10:58:51 +0100

    feat: switch to s3 remote state

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,27 +1,18 @@
 terraform {
   backend "s3" {
-    bucket                      = "gexec-terraform"
-    key                         = "github.tfstate"
-    region                      = "auto"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
-    use_path_style              = true
-    endpoints = {
-      s3 = "https://4fc42edfd414051cf85497affc49b987.r2.cloudflarestorage.com"
-    }
+    bucket = "gexec-terraform"
+    key    = "github"
+    region = "eu-central-1"
   }
 
   required_providers {
-    cloudflare = {
+    github = {
       source  = "integrations/github"
       version = "~> 6.5.0"
     }
   }
 
-  required_version = ">= 1.0"
+  required_version = ">= 1.9"
 }
 
 provider "github" {
